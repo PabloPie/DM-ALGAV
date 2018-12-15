7dd53e6d89ca11e598ab2b072abfc5fe3c59d986
Pablo <ppiedrafita95@gmail.com>
2018-12-15 14:51:50 +0100

    Test consIter importing files using Word128

--- src/minHeapTest.hs
+++ src/minHeapTest.hs
@@ -1,27 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
-import qualified MinHeapTree as T
-import qualified MinHeapArray as A
 import Control.Exception
 import System.Clock
+import System.Environment
 import Formatting.Clock
 import Formatting
+import System.IO
+import Control.Monad
+import Data.DoubleWord
 
+import qualified MinHeapTree as T
+import qualified MinHeapArray as A
+
+f :: [String] -> [Word128]
+f = map read
+
+-- give the file location as program argument, e.g. ../data/jeu_1_nb_cles_100.txt
+-- if given no arguments or more than 1, it won't show a comprehensible error
 main :: IO()
 main = do
-    lastVal <- getLine
-    let x = (read lastVal :: Int)
+    [filename] <- getArgs
+
+    let list = []
+    handle <- openFile filename ReadMode
+    contents <- hGetContents handle
+    let singlewords = words contents
+        list = f singlewords
+    print list
+    hClose handle
+
     start <- getTime Monotonic
-    evaluate(T.consIter [0,1..x])
+    evaluate(T.consIter list)
     end <- getTime Monotonic
     fprint (timeSpecs % "\n") start end
 
+
     start <- getTime Monotonic
-    a <- A.consIter [0,1..x]
-    -- b <- A.consIter [20,19..11]
-    -- c <- A.union a b
+    a <- A.consIter list
     end <- getTime Monotonic
     fprint (timeSpecs % "\n") start end
-
-    -- A.printAll c
-
-
